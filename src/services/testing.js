c005bf961caf8818199b4377e201622d122e9dac
Rafael Camargo <hello@rafaelcamargo.com>
2023-01-22 18:50:37 -0300

    chore: add testing library to the project

---
+++ src/services/testing.js
@@ -0,0 +1,14 @@
+import userEvent from '@testing-library/user-event'
+export * from '@testing-library/dom';
+
+export function render(html){
+  document.body.innerHTML = html;
+  return {
+    element: document.body.firstChild,
+    user: userEvent.setup()
+  };
+}
+
+export function teardown(){
+  document.body.firstChild.remove();
+}
